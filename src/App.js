5953648095e6e6f04fee54b2a3e8dd073c0aaa9a
Jérémy Luu <73641462+Jeyluu@users.noreply.github.com>
2021-10-14 17:42:33 +0200

    Main page ok

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import Footer from './Components/Footer/Footer';
 import Nav from './Components/Navbar/Navbar'
 import Home from './Containers/Home/Home';
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Nav />
       <Home />
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Footer.css'
+
+export default function Footer() {
+    return (
+        <div className="foot">
+            <ul>
+                <li className="links"> <a href="#">Follow Us</a> </li>
+                <li className="links"> <a href="#">Contact Us</a></li>
+                <li className="links"> <a href="#">Legals</a></li>
+            </ul>
+        </div>
+    )
+}
