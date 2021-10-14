95e55f86f926cc0e4eaaafc3ee0c1f0900492e7c
Jérémy Luu <73641462+Jeyluu@users.noreply.github.com>
2021-10-14 17:05:04 +0200

    body ok

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import Nav from './Components/Navbar/Navbar'
+import Home from './Containers/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      
+      <Nav />
+      <Home />
     </div>
   );
 }
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Navbar.css'
+
+export default function Navbar() {
+    return (
+        <div className="navbar">
+            <ul>
+                <li className="nav-link">Home</li>
+                <li className="nav-link">Characters</li>
+                <li className="nav-link">Armors</li>
+            </ul>
+        </div>
+    )
+}
